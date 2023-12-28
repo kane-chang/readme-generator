772ed57ab7cae8d2de081255bc3276435b717c04
Kane Chang <kane.chang.fl@gmail.com>
2023-12-28 21:15:19 +0000

    added readme template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,49 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
 
+  ![Static Badge](https://img.shields.io/badge/license-MIT-blue)
+  
+  ${data.description}
+  
+  ## Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [Contributing](#Contributing)
+  4. [License](#License)
+  
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`bash
+
+  ${data.installCommand}
+
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  To run tests, run the following command:
+  
+  \`\`\`bash
+  ${data.installCommand}
+  \`\`\`
+  
+  ## Contributing
+  
+  ${data.contribution}
+  
+  ## License
+  
+  [MIT](https://choosealicense.com/licenses/mit/)
+  
+  ## Questions
+  
+  To get in touch, contact me at ${data.email}. You can find my other works at [${data.username}](https://github.com/${data.username})
 `;
 }
 
